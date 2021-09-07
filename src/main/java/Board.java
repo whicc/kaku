d4bf1762c231ff28f70e42b48ef49995d0ef9e06
Concavebark <concavebarkplaysgames@yahoo.com>
2021-09-07 05:44:18 -0400

    9/7/21 Update
    Update Includes:
     - All math related to move legality checks
     - Improved comments for cleanliness and clarity 
     - Issues and or concerns in the form of TODO:'s and comments
    
    ~Check method isKingInCheckMate possible issue above in a comment.~

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,14 +1,24 @@
 import static java.lang.Math.abs;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+/** TODO: Change movePiece from boolean to integer with return codes:
+ * 0 = No errors; move made
+ * 1 = invalid move
+ * 2 = white wins
+ * 3 = black wins
+ **/
 
 public class Board {
 
     private Piece boardState[][] = new Piece[8][8];
     private Piece tempBoard[][] = new Piece[8][8];
+    /*  Intellij keeps noting that the above declarations are of "C-style array declaration" the two declarations below may be a possible fix to this.
+        Also, it seems memory inefficient to have a carbon copy of boardState stored in another variable, although this could obviously be due to Java wackiness.
+        private Piece[][] boardState = new Piece[8][8];
+        private Piece[][] tempBoard = new Piece[8][8];  */
 
-
-    public boolean movePiece(int oldY, int oldX, int newY, int newX){
+    public int movePiece(int oldY, int oldX, int newY, int newX){
 
         Piece oldPiece = boardState[oldY][oldX];
 
@@ -18,10 +28,10 @@ public class Board {
             tempBoard[oldY][oldX] = new Piece();
             tempBoard[newY][newX] = oldPiece;
 
-            int kingX = 0; // had to intialize with a value because java was pissed
+            int kingX = 0; // had to initialize with a value because java was pissed
             int kingY = 0;
 
-            for(int y = 0; y < 7; y++){ // this is bad code
+            for(int y = 0; y < 7; y++){ // this is bad code //TODO: Make this good code... soon(tm)
 
                 for(int x = 0; x < 7; x++){
 
@@ -40,28 +50,26 @@ public class Board {
                 boardState[oldY][oldX] = new Piece();
                 boardState[newY][newX] = oldPiece;
 
-                return true;
+                return 0; //was previously true
             }
 
-            return false;
+            return 1; //was false
         }
 
-        return false;
+        return 1; //was false
     }
 
     private ArrayList<int[]> calcValidKnightMoves(int oldX, int oldY) {
         int[] currentLoc = {oldX, oldY};
-        ArrayList<int[]> validGlobalCoords = new ArrayList<>(); //should double-check if this clears previous list from memory or if it has to be told to
+        ArrayList<int[]> validGlobalCoords = new ArrayList<>();
         validGlobalCoords.clear(); //Almost certain this isn't required, but I am clearing it just to make sure specifically for mem management
         int[][] validLocalCoords = {{-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}, {-2,1}};
         int[] tempLoc = new int[2];
         for (int i = 0; i<validLocalCoords.length; i++) {
-            for(int a = 0; a<2; a++) { // A should NEVER be 2 only 0 and 1.
+            for(int a = 0; a<2; a++) { // variable a should NEVER be 2 only 0 and 1.
                 if ((validLocalCoords[i][a] += currentLoc[a]) >=  0 && (validLocalCoords[i][a] += currentLoc[a]) <= 7){
                     tempLoc[a] = validLocalCoords[i][a] += currentLoc[a]; //this line ensures that the math is done on both X and Y and is put into tempLoc in the correct order
                 }
-                //tempLoc[0] = validLocalCoords[i][a] += oldX; this is where the whole function could've broken down and been completely useless
-                //tempLoc[1] = validLocalCoords[i][a] += oldY; surprised I didn't realize this while writing it tbh
                 validGlobalCoords.add(tempLoc);
             }
         }
@@ -82,7 +90,6 @@ public class Board {
 
         if(oldPiece.getAffiliation() != newPiece.getAffiliation()){
 
-            //boolean isValid = true;
             int distanceX = abs(newX - oldX);
             int distanceY = abs(newY - oldY);
 
@@ -94,21 +101,21 @@ public class Board {
 
                 case ROOK:
                     if (distanceX > 0 && distanceY > 0) {
-                    	return false;
+                        return false;
                     }
                     if (distanceX > 0) {
                         for (int i = oldX + (newX-oldX)/(distanceX);  i>0 || i<7; i+=(newX-oldX)/(distanceX) ) {
                             if (boardState[oldY][i].getType() != PieceInfo.BLANK && i!=newX) {
-                            	return false;
-                            	}//check if something here
+                                return false;
+                            }//this if statement checks if something is in the path between the rook and where the selection if the user's selection is on the X axis
                             if (i==newX) break;
                         }
                     }// be gay
                     if (distanceY > 0) {
                         for (int i = oldY + (newY-oldY)/(distanceY);  i>0 || i<7; i+=(newY-oldY)/(distanceY) ) {
                             if (boardState[i][oldX].getType() != PieceInfo.BLANK && i!=newY) {
-                            	return false;
-                            	}//check if something here
+                                return false;
+                            }//this if statement checks if something is in the path between the rook and where the user selected if the selection is on the Y axis
                             if (i==newY) break;
                         }
                     }// be straight
@@ -120,23 +127,23 @@ public class Board {
                     ArrayList<int[]> locStorage = new ArrayList<>();
                     locStorage.clear(); //another just-in-case type deal where I'm pretty sure it's unnecessary but to make sure mem is handled well
                     locStorage = calcValidKnightMoves(oldX,oldY);
-                    /*  need to check each provided location on the board and see if it's blank, not blank, or of same affiliation, if it is,
-                        see if it's what the user selected */
+                    boolean validUserSelection = false;
                     for (int i = 0; i<locStorage.size(); i++) {
-                        if (locStorage.get(i) == userSel) {
-                            //this just checks to see if what the user selected IS one of the available legal locations for the knight pretty sure this is technically unnecessary
-                        } else {
-                            return false; // this isn't done correctly I'm pretty sure, need to check all and if none are equal to what the user selected, THEN return false
+                        if (Arrays.equals(locStorage.get(i), userSel)) {
+                            validUserSelection = true;//this just checks to see if what the user selected IS one of the available legal locations for the knight pretty sure this is technically unnecessary
                         }
                         if (boardState[locStorage.get(i)[0]][locStorage.get(i)[1]].getAffiliation() == boardState[oldX][oldY].getAffiliation()) { //check if both pieces are of same affiliation, therefore making the move invalid
-                            return false; // this should be setup correctly I'm pretty sure.
+                            return false;
                         }
+                    }
+                    if (!validUserSelection) {
+                        return false; //this is just the flag that is needed to ensure we check through all valid knight move locations and make sure the user selected a spot the knight can legally go to
                     }
                     break;
                 case BISHOP:
                     if(newX-oldX != newY-oldY)
                         return false; // x and y have to be the same
-
+                    //TODO: check to see if the equation (newX-oldX)/((newX-oldX)*-1) could just be declared above switch case and re-used
                     direction = (newX-oldX)/((newX-oldX)*-1); // get the direction to iterate
 
                     checkX = oldX + direction;
@@ -182,35 +189,35 @@ public class Board {
 
                         if((distanceX == 0 && distanceY > 0) || (distanceY == 0 && distanceX > 0)){ // sideways move
 
-                            if (distanceX > 0 && distanceY > 0) { //lmao
+                            if (distanceX > 0 && distanceY > 0) {
                                 return false;
-                            }
+                            }// it appears the code below is re-used from rook math, could possibly be annexed and popped into a separate  method and used for both rook and queen
                             if (distanceX > 0) {
                                 for (int i = oldX + (newX-oldX)/(distanceX);  i>0 || i<7; i+=(newX-oldX)/(distanceX) ) {
                                     if (boardState[oldY][i].getType() != PieceInfo.BLANK && i!=newX) {
                                         return false;
-                                    }//check if something here
+                                    }//this if statement checks if something is in the path between the rook and where the selection if the user's selection is on the X axis
                                     if (i==newX) break;
                                 }
-                            }// be gay
+                            }
                             if (distanceY > 0) {
                                 for (int i = oldY + (newY-oldY)/(distanceY);  i>0 || i<7; i+=(newY-oldY)/(distanceY) ) {
                                     if (boardState[i][oldX].getType() != PieceInfo.BLANK && i!=newY) {
                                         return false;
-                                    }//check if something here
+                                    }//this if statement checks if something is in the path between the rook and where the selection if the user's selection is on the Y axis
                                     if (i==newY) break;
                                 }
-                            }// be straight
+                            }
                         }else
                             return false; // neither move types selected
                     }
                     break;
                 case KING:
                     if (distanceX > 1 || distanceY > 1) {
-                        return false; // if the king is requested to move further than 1 tile, tis invalid move
+                        return false; // if the king is requested to move further than 1 tile, the move is invalid
                     }
                     if (newPiece.getAffiliation() == oldPiece.getAffiliation()) {
-                        return false; // the king may not step on his underlings.
+                        return false; // the king cannot move to a position currently occupied by a piece of the same affiliation
                     }
 
                     for(int y = 0; y < 8; y++){
@@ -238,7 +245,7 @@ public class Board {
                             return false;
                         }
                     }
-
+                    // I feel like the math here is actually done incorrectly, since pawns cannot attack directly forward but only to the top right and top left of themselves
                     if(distanceY == 1){ // Taking a piece
                         if(distanceX > 0){
                             if(newPiece.getType() == PieceInfo.BLANK)
@@ -283,9 +290,9 @@ public class Board {
 
         return false; // The king is not in check
     }
-
+    // I do think that this can be done in a way that is technically more efficient, we constantly run through the same method multiple times in one return statement, I also believe this will be completely broken once we move to valid moves returning integers instead of booleans.
     public boolean isKingInCheckMate(int kingX, int kingY){ // this is terrible, yes, but its efficient. So suck my nuts.
-        //                                  left                          up                                                                                                       top left                                                                                                  right                                                                                                   bottom                                                                                               bottom right                                                                                             bottom left                                                                                                   top right
+        //                                  left                          up                                                                                                       top left                                                                                                  right                                                                                                   below                                                                                               bottom right                                                                                             bottom left                                                                                                   top right
         return (isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX, kingY-1) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX-1, kingY-1) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX+1, kingY) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX, kingY+1) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX+1, kingY+1) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX-1, kingY+1) && isMoveValid(kingX, kingY, kingX-1, kingY) == isMoveValid(kingX, kingY, kingX+1, kingY-1) && !isMoveValid(kingX, kingY, kingX+1, kingY-1));
     }
 }
